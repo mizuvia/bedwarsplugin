9a198510aa0dc61acfedca7e2c2c36f4b2734415
BuildTools <unconfigured@null.spigotmc.org>
2021-12-14 18:33:49 +0300

    Fix bed action

--- src/main/java/events/onPlayerInteract.java
+++ src/main/java/events/onPlayerInteract.java
@@ -121,8 +121,11 @@ public class onPlayerInteract extends SimpleListener implements Listener, EventE
 
             Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this.getPlugin(), () -> getPlugin().getPlayers().get(e.getPlayer().getName()).setUnderMilk(false), 1200L);
         }
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+        	Bukkit.broadcastMessage(e.getClickedBlock().getType().name());
+        }
         if (e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getClickedBlock().getType().name().endsWith("_BED")) {
-        	e.setCancelled(false);
+        	e.setCancelled(true);
         }
     }
 }
