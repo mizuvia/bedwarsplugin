d919f3cd15d681356c5afede0d088ebd5d54bd06
BuildTools <unconfigured@null.spigotmc.org>
2021-12-18 22:24:24 +0300

    Bug fix

---
+++ src/main/java/util/LastDamager.java
@@ -0,0 +1,22 @@
+package util;
+
+public class LastDamager {
+
+	private String name;
+	private long time;
+	
+	public LastDamager() {
+		this.name = "";
+		this.time = 0;
+	}
+	
+	public String get() {
+		return time + 10000 < System.currentTimeMillis() ? name : null;
+	}
+	
+	public void put(String name) {
+		this.name = name;
+		this.time = System.currentTimeMillis();
+	}
+	
+}
