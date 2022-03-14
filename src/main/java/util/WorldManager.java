e92022b550d4a9dcd74d8ad9dcad98b4ce3e1a02
Hoshinon <mwadman2018@gmail.com>
2022-03-14 16:11:42 +0200

    fixing bug with screamer armor stand

--- src/main/java/util/WorldManager.java
+++ src/main/java/util/WorldManager.java
@@ -11,11 +11,11 @@ import org.bukkit.inventory.ItemStack;
 
 public class WorldManager {
     public static boolean canDropResource(Location location, Material material, int maxAmount){
-        ArmorStand ent = (ArmorStand) Bukkit.getWorld("world").spawnEntity(location.add(0, -3, 0), EntityType.ARMOR_STAND);
+        ArmorStand ent = (ArmorStand) Bukkit.getWorld("world").spawnEntity(new Location(location.getWorld(), location.getX(), location.getY() - 2, location.getZ()), EntityType.ARROW);
         ent.setInvisible(true);
 
         int itemsInArea = 0;
-        for(Entity entity : ent.getNearbyEntities(3, 3, 3)){
+        for(Entity entity : ent.getNearbyEntities(5, 5, 5)){
             if(entity instanceof Item && ((Item) entity).getItemStack().getType() == material) itemsInArea += ((Item) entity).getItemStack().getAmount();
         }
         ent.remove();
