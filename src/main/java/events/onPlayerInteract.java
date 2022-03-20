30c6a547fbd45d51299f9b5ddc79b858c92f169c
Hoshinon <mwadman2018@gmail.com>
2022-03-20 18:04:49 +0200

    making water bucket to remove after pouring water

--- src/main/java/events/onPlayerInteract.java
+++ src/main/java/events/onPlayerInteract.java
@@ -132,6 +132,10 @@ public class onPlayerInteract extends SimpleListener implements Listener, EventE
                     Bukkit.getScheduler().runTaskLater(plugin, fireball::remove, 400);
                 }
             }
+            case WATER_BUCKET -> {
+                if (e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+                consumeItem(ShopItem.WATER_BUCKET);
+            }
             case MILK_BUCKET -> {
                 if(!Utils.isRightClick(e.getAction())) return;
 
